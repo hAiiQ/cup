39b85e22e1bf9a7a22812b6fee875701d3947ac3
hAiQ <haiq@example.com>
2025-08-29 03:03:48 +0200

    Vereinfachtes Glücksrad - Komplette Neuschreibung
    -  8-Sekunden Spindauer mit sanfter Verlangsamung
    -  Vereinfachte mathematische Logik ohne komplexe Offsets
    -  Gewinner wird vorab bestimmt und landet immer unter dem Pfeil
    -  Detaillierte Console-Logs zur Verifikation
    -  Klarere Canvas-Darstellung mit besserer UX
    -  Entfernt fehleranfällige Winkel-Berechnungen
    
    Das Wheel sollte jetzt 100% akkurat sein!

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,32 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-57586abe07dbe25a.js","static/chunks/fd9d1056-0b9695e3ab5c31f0.js","static/chunks/117-bb9bda925dcbef42.js","static/chunks/main-app-a3794d702fc9c3c4.js"],pages:{"/_app":["static/chunks/webpack-57586abe07dbe25a.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-fd4e06312f01a3c0.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-57586abe07dbe25a.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-fd4e06312f01a3c0.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
